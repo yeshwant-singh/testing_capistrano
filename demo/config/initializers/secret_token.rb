b9f44806ff8d8c5b193821d1f83611e6339d9823
yeshwant singh <yeshwant007@gmail.com>
2018-04-10 14:38:16 +0530

    initial commit

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'f086ffa37ee4088134fabda2c35a18531fe29338bc4cb3398036975f3d6e197cc839c7f873da171ece8e11333369340a39d225b6964e906976b4624cde7d74e6'
---
+++ demo/db/migrate/20180410090323_books.rb
@@ -0,0 +1,7 @@
+class Books < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
